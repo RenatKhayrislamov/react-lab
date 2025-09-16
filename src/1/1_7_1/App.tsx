211f6fb81af9e659565d0fbe8495be245cb5ebf0
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-16 18:16:36 +0500

    feat: 1_7_1 split list in two

--- src/1/1_7_1/App.tsx
+++ src/1/1_7_1/App.tsx
@@ -17,23 +17,47 @@ export type Person = {
 }
 
 export default function List() {
-    const listItems = people.map((person) => (
-        <li key={person.id}>
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
+  );
+  const everyoneElse = people.filter(person =>
+    person.profession !== 'chemist'
+  );
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemists</h2>
+      <ul>
+        {chemists.map(person =>
+          <li key={person.id}>
             <img
-                src={getImageUrl(person)}
-                alt={person.name}
+              src={getImageUrl(person)}
+              alt={person.name}
             />
             <p>
-                <b>{person.name}:</b>
-                {' ' + person.profession + ' '}
-                known for {person.accomplishment}
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
             </p>
-        </li>
-    ));
-    return (
-        <article>
-            <h1>Scientists</h1>
-            <ul>{listItems}</ul>
-        </article>
-    );
+          </li>
+        )}
+      </ul>
+      <h2>Everyone Else</h2>
+      <ul>
+        {everyoneElse.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+    </article>
+  );
 }
