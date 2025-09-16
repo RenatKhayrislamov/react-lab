bbb617a550f93ceb1279072fd6f44f1a199e9e77
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-16 19:27:09 +0500

    fix: 1_8_2 fix a broken profile

--- src/1/1_8_2/Profile.tsx
+++ src/1/1_8_2/Profile.tsx
@@ -2,28 +2,25 @@ import { Person } from './App.js';
 import Panel from './Panel.js';
 import { getImageUrl } from './utils.js';
 
-let currentPerson: Person
-
 export default function Profile({ person }: { person: Person }) {
-    currentPerson = person;
     return (
         <Panel>
-            <Header />
-            <Avatar />
+            <Header person={person} />
+            <Avatar person={person} />
         </Panel>
     );
 }
 
-function Header() {
-    return <h1>{currentPerson.name}</h1>;
+function Header({ person }: { person: Person }) {
+    return <h1>{person.name}</h1>;
 }
 
-function Avatar() {
+function Avatar({ person }: { person: Person }) {
     return (
         <img
             className="avatar"
-            src={getImageUrl(currentPerson)}
-            alt={currentPerson.name}
+            src={getImageUrl(person)}
+            alt={person.name}
             width={50}
             height={50}
         />
