1d2004c564039c16afffeb5d4532e5819e0703b0
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-16 17:05:39 +0500

    feat: 1_5_3 extract Card component and use children prop

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -3,27 +3,32 @@
   Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
 */
 
+function Card({ children, title }: { children: React.ReactNode, title: string }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
+  );
+}
+
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
-          <h1>About</h1>
-          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
+      <Card title="Photo">
+        <img
+          className="avatar"
+          src="OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card title="About">
+        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+      </Card>
     </div>
   );
 }
