01eccdc438a7b5feff218d1bb183570293ebc76f
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-17 12:16:03 +0500

    fix: 2_2_4 remove unnecessary state

--- src/2/2_2_4/App.tsx
+++ src/2/2_2_4/App.tsx
@@ -10,10 +10,8 @@
 import { useState } from 'react';
 
 export default function FeedbackForm() {
-    const [name, setName] = useState('');
-
     function handleClick() {
-        setName(prompt('What is your name?')??"");
+        const name = prompt('What is your name?');
         alert(`Hello, ${name}!`);
     }
 
