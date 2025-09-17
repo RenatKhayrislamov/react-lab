bf8ee0a3cd7c34d8f22e9f8d28eabeb11a8f351a
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-17 13:32:04 +0500

    feat: 2_6_3 update an object with Immer

--- src/2/2_6_3/App.tsx
+++ src/2/2_6_3/App.tsx
@@ -20,20 +20,21 @@ const initialPosition = {
 };
 
 export default function Canvas() {
-    const [shape, setShape] = useState({
+    const [shape, updateShape] = useImmer({
         color: 'orange',
         position: initialPosition,
     });
 
     function handleMove(dx: number, dy: number) {
-        shape.position.x += dx;
-        shape.position.y += dy;
+        updateShape(draft => {
+            draft.position.x += dx;
+            draft.position.y += dy;
+        });
     }
 
     function handleColorChange(e: any) {
-        setShape({
-            ...shape,
-            color: e.target.value,
+        updateShape(draft => {
+            draft.color = e.target.value;
         });
     }
 
