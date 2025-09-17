45f3b559176d81ca280d8d8aec129c377c1a2e91
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-17 19:00:56 +0500

    feat: 3_1_1 add and remove a CSS class

--- src/3/3_1_1/App.tsx
+++ src/3/3_1_1/App.tsx
@@ -5,11 +5,30 @@
     Визуально вы должны увидеть, что щелчок на изображении удаляет фиолетовый фон и выделяет границу изображения. Щелчок за пределами изображения выделяет фон, но убирает выделение границы изображения.
 */
 
+import { useState } from 'react';
+
 export default function Picture() {
+    const [isActive, setIsActive] = useState(false);
+
+    let backgroundClassName = 'background';
+    let pictureClassName = 'picture';
+    if (isActive) {
+        pictureClassName += ' picture--active';
+    } else {
+        backgroundClassName += ' background--active';
+    }
+
     return (
-        <div className="background background--active">
+        <div
+            className={backgroundClassName}
+            onClick={() => setIsActive(false)}
+        >
             <img
-                className="picture"
+                onClick={e => {
+                    e.stopPropagation();
+                    setIsActive(true);
+                }}
+                className={pictureClassName}
                 alt="Rainbow houses in Kampung Pelangi, Indonesia"
                 src="/5qwVYb1.jpg"
             />
