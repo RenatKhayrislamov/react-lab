1176bfec68be67e3a5299e55af0f250586e20b0f
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-18 11:56:13 +0500

    fix: 3_4_1 fix disappearing input text

--- src/3/3_4_1/App.tsx
+++ src/3/3_4_1/App.tsx
@@ -7,23 +7,19 @@ import { useState } from 'react';
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
+  return (
+    <div>
+      {showHint && <p><i>Hint: Your favorite city?</i></p>}
+      <Form />
+      {showHint ? (
         <button onClick={() => {
           setShowHint(false);
         }}>Hide hint</button>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <Form />
-      <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
+      ) : (
+        <button onClick={() => {
+          setShowHint(true);
+        }}>Show hint</button>
+      )}
     </div>
   );
 }
