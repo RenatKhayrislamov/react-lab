c868f083729b3a02ed4785fe72482b6a4d1c24fd
RenatKhayrislamov <qqss_11@mail.ru>
2025-09-20 17:54:07 +0500

    feat: 4_8_3 extract useInterval out of useCounter

--- src/4/4_8_3/useInterval.ts
+++ src/4/4_8_3/useInterval.ts
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 
 export function useInterval(callback: () => void, delay: number) {
-    // Тут нужно реализовать хук
+    useEffect(() => {
+        const id = setInterval(callback, delay);
+        return () => clearInterval(id);
+    }, [delay]);
 }
